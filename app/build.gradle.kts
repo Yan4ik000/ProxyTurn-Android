d66e9ec93c2ca99420900a626cc6bf1069994fb3
Yan4ik000 <truyan4ik@gmail.com>
2026-08-07 13:27:38 +0300

    version bump v1.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.wdtt.client"
         minSdk = 28
         targetSdk = 35
-        versionCode = 124
-        versionName = "1.2.4"
+        versionCode = 130
+        versionName = "1.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
